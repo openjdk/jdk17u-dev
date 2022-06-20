c579e624f8fb41fafc9b081eeec91cef88dfb400
Emanuel Peter <epeter@openjdk.org>
2022-06-20 14:29:06 +0000

    Backport 302a6c068dcbb176381b1535baf25547079c9b06

--- test/hotspot/jtreg/compiler/rangechecks/TestRangeCheckSmearing.java
+++ test/hotspot/jtreg/compiler/rangechecks/TestRangeCheckSmearing.java
@@ -28,8 +28,9 @@
  * @library /test/lib /
  * @modules java.base/jdk.internal.misc
  *          java.management
- * @build jdk.test.whitebox.WhiteBox
- * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @build sun.hotspot.WhiteBox
+ * @requires vm.flavor == "server" & (vm.opt.TieredStopAtLevel == null | vm.opt.TieredStopAtLevel == 4)
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller sun.hotspot.WhiteBox
  * @run main/othervm -ea -Xmixed -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
  *                   -XX:-BackgroundCompilation -XX:-UseOnStackReplacement
  *                   -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockExperimentalVMOptions -XX:+EagerJVMCI
