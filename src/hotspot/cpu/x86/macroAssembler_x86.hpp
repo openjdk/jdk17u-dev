379b6026cc52a37bb8fbe3517f54e59788602ee5
Evgeny Astigeevich <eastig@amazon.com>
2022-10-10 16:01:11 +0000

    Backport 351560414d7ddc0694126ab184bdb78be604e51f

--- src/hotspot/cpu/x86/macroAssembler_x86.hpp
+++ src/hotspot/cpu/x86/macroAssembler_x86.hpp
@@ -842,6 +842,8 @@ class MacroAssembler: public Assembler {
   // Emit the CompiledIC call idiom
   void ic_call(address entry, jint method_index = 0);
 
+  void emit_static_call_stub();
+
   // Jumps
 
   // NOTE: these jumps tranfer to the effective address of dst NOT
