20bbf1abd7314745db58ec72ff0a059192a7a507
Goetz Lindenmaier <goetz.lindenmaier@sap.com>
2023-01-02 21:57:44 +0100

    fix merge

--- src/hotspot/share/opto/memnode.cpp
+++ src/hotspot/share/opto/memnode.cpp
@@ -1196,9 +1196,6 @@ bool LoadNode::is_instance_field_load_with_local_phi(Node* ctrl) {
 //------------------------------Identity---------------------------------------
 // Loads are identity if previous store is to same address
 Node* LoadNode::Identity(PhaseGVN* phase) {
-  if (has_pinned_control_dependency()) {
-    return this;
-  }
   // If the previous store-maker is the right kind of Store, and the store is
   // to the same address, then we are equal to the value stored.
   Node* mem = in(Memory);
