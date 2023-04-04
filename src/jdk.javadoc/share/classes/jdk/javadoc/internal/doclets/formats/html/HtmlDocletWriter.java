f2fccf04fd5f2856f407d5ccf799cbf446a6446d
Takakuri <k.takakuri@fujitsu.com>
2023-04-04 15:54:12 +0900

    Backport bb95dda0ac5032e4df582f2903dcbbbbaef9e8c4

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HtmlDocletWriter.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HtmlDocletWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1704,7 +1704,8 @@ public class HtmlDocletWriter {
         if (lower.startsWith("mailto:")
                 || lower.startsWith("http:")
                 || lower.startsWith("https:")
-                || lower.startsWith("file:")) {
+                || lower.startsWith("file:")
+                || lower.startsWith("ftp:")) {
             return text;
         }
         if (text.startsWith("#")) {
