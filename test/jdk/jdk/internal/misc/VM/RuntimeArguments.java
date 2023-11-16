df2847b57321e8797850e97720a07d311471d14a
Mandy Chung <mchung@openjdk.org>
2023-11-16 22:40:22 +0000

    Backport 1588dd934ce4e00a060e329b80f721d894559597

--- test/jdk/jdk/internal/misc/VM/RuntimeArguments.java
+++ test/jdk/jdk/internal/misc/VM/RuntimeArguments.java
@@ -23,11 +23,12 @@
 
 /**
  * @test
- * @summary Basic test of VM::getRuntimeArguments
+ * @requires vm.flagless
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
  *          jdk.zipfs
  * @run testng RuntimeArguments
+ * @summary Basic test of VM::getRuntimeArguments
  */
 
 import java.util.Arrays;
@@ -40,7 +41,30 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class RuntimeArguments {
-    static final String TEST_CLASSES = System.getProperty("test.classes");
+    static final List<String> VM_OPTIONS = getInitialOptions();
+
+    /*
+     * Read jdk/internal/vm/options resource from the runtime image.
+     * If present, the runtime image was created with jlink --add-options and
+     * the java launcher launches the application as if
+     *   $ java @options <app>
+     * The VM options listed in the jdk/internal/vm/options resource file
+     * are passed to the VM.
+     */
+    static List<String> getInitialOptions() {
+        ModuleReference mref = ModuleFinder.ofSystem().find("java.base").orElseThrow();
+        try (ModuleReader reader = mref.open()) {
+            InputStream in = reader.open("jdk/internal/vm/options").orElse(null);
+            if (in != null) {
+                // support the simplest form for now: whitespace-separated
+                return List.of(new String(in.readAllBytes()).split("\s"));
+            } else {
+                return List.of();
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
     @DataProvider(name = "options")
     public Object[][] options() {
@@ -82,14 +106,14 @@ public class RuntimeArguments {
 
     @Test(dataProvider = "options")
     public void test(List<String> args, List<String> expected) throws Exception {
-        // launch a test program
-        // $ java <runtime-arguments> -classpath <cpath> RuntimeArguments <expected>
-
-        Stream<String> options = Stream.concat(args.stream(),
-            Stream.of("-classpath", TEST_CLASSES, "RuntimeArguments"));
-
-        ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(
-            Stream.concat(options, expected.stream())
+        // launch a test program with classpath set by ProcessTools::createLimitedTestJavaProcessBuilder
+        // $ java <args> RuntimeArguments <vm_options> <expected>
+        Stream<String> options = Stream.concat(args.stream(), Stream.of("RuntimeArguments"));
+        ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(
+            // The runtime image may be created with jlink --add-options
+            // The initial VM options will be included in the result
+            // returned by VM.getRuntimeArguments()
+            Stream.concat(options, Stream.concat(VM_OPTIONS.stream(), expected.stream()))
                   .toArray(String[]::new)
         );
         ProcessTools.executeProcess(pb).shouldHaveExitValue(0);
