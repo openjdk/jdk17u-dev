0099108ce33d405cad6301a9676ad67675479b70
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2024-01-15 15:43:17 +0100

    Backport e3201d1d13433857a1b34ff0ca93f9ae1a4e22aa

--- test/jdk/java/util/Currency/ValidateISO4217.java
+++ test/jdk/java/util/Currency/ValidateISO4217.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,6 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 /*
  * @test
  * @bug 4691089 4819436 4942982 5104960 6544471 6627549 7066203 7195759
