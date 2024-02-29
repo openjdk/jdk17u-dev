f0c0f95c7d392afc0ef259eefc5176d91c5c9de2
Liang Mao <maoliang.ml@alibaba-inc.com>
2024-02-29 13:11:10 +0800

    Backport bde87895c8b1b9df198e3883d24cd9ea840efc98

--- src/hotspot/cpu/aarch64/assembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/assembler_aarch64.hpp
@@ -763,6 +763,8 @@ public:
 
   void adrp(Register Rd, const Address &dest, uint64_t &offset);
 
+  void prfm(const Address &adr, prfop pfop = PLDL1KEEP);
+
 #undef INSN
 
   void add_sub_immediate(Register Rd, Register Rn, unsigned uimm, int op,
@@ -1495,17 +1497,6 @@ public:
   INSN(ldrsh, 0b01, 0b10);
   INSN(ldrshw, 0b01, 0b11);
   INSN(ldrsw, 0b10, 0b10);
-
-#undef INSN
-
-#define INSN(NAME, size, op)                                    \
-  void NAME(const Address &adr, prfop pfop = PLDL1KEEP) {       \
-    ld_st2((Register)pfop, adr, size, op);                      \
-  }
-
-  INSN(prfm, 0b11, 0b10); // FIXME: PRFM should not be used with
-                          // writeback modes, but the assembler
-                          // doesn't enfore that.
 
 #undef INSN
 
