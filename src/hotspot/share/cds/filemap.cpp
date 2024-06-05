192d9c2770766bb2fbc6ba2fe468d9c7e4f3e635
Elif Aslan <elifasln@amazon.com>
2024-06-05 08:10:17 -0700

    Backport of 4ff6720573f9b55eb397d1aac9b398228faf2ceb

--- src/hotspot/share/cds/filemap.cpp
+++ src/hotspot/share/cds/filemap.cpp
@@ -1420,8 +1420,8 @@ size_t FileMapInfo::write_archive_heap_regions(GrowableArray<MemRegion> *heap_me
 
 void FileMapInfo::write_bytes(const void* buffer, size_t nbytes) {
   assert(_file_open, "must be");
-  size_t n = os::write(_fd, buffer, (unsigned int)nbytes);
-  if (n != nbytes) {
+  ssize_t n = os::write(_fd, buffer, (unsigned int)nbytes);
+  if (n < 0 || (size_t)n != nbytes) {
     // If the shared archive is corrupted, close it and remove it.
     close();
     remove(_full_path);
