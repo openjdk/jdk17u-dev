5bc41db9d277f22b2ddddf368e01c4711a550c3b
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2025-06-17 18:00:57 +0200

    Undo spurious change

--- test/hotspot/jtreg/vmTestbase/metaspace/gc/watermark_0_1/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/metaspace/gc/watermark_0_1/TestDescription.java
@@ -30,6 +30,9 @@
  * VM Testbase keywords: [nonconcurrent, no_cds]
  *
  * @comment Don't run test in configurations where we can't reliably count number of metaspace triggered GCs
+ * @requires vm.gc != null | !vm.opt.final.ClassUnloadingWithConcurrentMark
+ * @requires vm.gc != "G1" | !vm.opt.final.ClassUnloadingWithConcurrentMark
+ * @requires vm.gc != "Z"
  * @requires vm.compMode != "Xcomp"
  * @library /vmTestbase /test/lib
  * @run main/othervm
