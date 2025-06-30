4122a9c0748876a08b074942f7998eb69041fedb
Taizo Kurashige <kurashige.taizo@fujitsu.com>
2025-06-30 16:14:30 +0900

    Backport 4458719a108f45d3744d47a6ea081fe9ec3e675e

--- test/jdk/tools/jpackage/windows/Win8301247Test.java
+++ test/jdk/tools/jpackage/windows/Win8301247Test.java
@@ -24,7 +24,6 @@
 import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
