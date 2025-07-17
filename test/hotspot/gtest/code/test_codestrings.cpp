9cb3b0b45301f8828fa20dbdd25e240036a64b7e
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2025-07-17 11:51:38 +0200

    fix automatic resolve

--- test/hotspot/gtest/code/test_codestrings.cpp
+++ test/hotspot/gtest/code/test_codestrings.cpp
@@ -25,8 +25,6 @@
 
 #ifndef PRODUCT
 #ifndef ZERO
-// Neither ppc nor s390 compilers use code strings.
-#if !defined(PPC) && !defined(S390)
 
 #include "asm/macroAssembler.inline.hpp"
 #include "compiler/disassembler.hpp"
@@ -270,6 +268,5 @@ TEST_VM(codestrings, validate)
     buffer_blob_test();
 }
 
-#endif // not S390 not PPC
 #endif // not ZERO
 #endif // not PRODUCT
